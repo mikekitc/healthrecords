634ba894a9eb695ad967d11362d1bb61eb7aef30
Michael Kitchell <mike@mikekitchell.com>
2013-10-05 15:34:39 -0700

    first commit

---
+++ Health Records/HealthProfile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Health_Records
+{
+    class HealthProfile
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public double height { get; set; }
+        public double weight { get; set; }
+
+        public HealthProfile(string firstName, string lastName, string gender, DateTime dateOfBirth, double height, double weight)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.dateOfBirth = dateOfBirth;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - dateOfBirth.Year;
+            if (dateOfBirth.AddYears(age) > now)
+                age--;
+            return age;
+        }
+
+        public int MaxHeartRate()
+        {
+            int maxHeartRate = 220 - this.CalculateAge(dateOfBirth);
+            return maxHeartRate;
+        }
+
+        public double MinTargetHeartRate()
+        {
+            double minTargetHeartRate = 0.5 * this.MaxHeartRate();
+            return Math.Floor(minTargetHeartRate);
+        }
+
+        public double MaxTargetHeartRate()
+        {
+            double maxTargetHeartRate = 0.85 * this.MaxHeartRate();
+            return Math.Floor(maxTargetHeartRate);
+        }
+
+        public string BMI()
+        {
+            double bodyMassIndex = ((weight * 703) / (height * height));
+            string value;
+
+            if (bodyMassIndex >= 30)
+                value = "Obese.";
+            else
+                if (bodyMassIndex >= 25)
+                    value = "Overweight.";
+                else
+                    if (bodyMassIndex >= 18.5)
+                        value = "Normal.";
+                    else
+                        value = "Underweight.";
+
+            return bodyMassIndex.ToString("n1") + " - " + value;
+        }
+
+        public string CalculateHeight()
+        {
+            double inches = height % 12;
+            double feet = height / 12;
+            return Math.Floor(feet) + "'" + inches + "\"";
+        }
+    }
+}
---
+++ Health Records/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Health_Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Gender: ");
+            string gender = Console.ReadLine();
+            Console.Write("Birthdate (MM-DD-YYYY): ");
+            DateTime dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Height (in inches): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Weight (in pounds): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            HealthProfile healthProfile = new HealthProfile(firstName, lastName, gender, dateOfBirth, height, weight);
+
+            Console.Clear();
+
+            Console.WriteLine("USER INFORMATION\n------------------------");
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.WriteLine("Gender: " + gender);
+            Console.WriteLine("Date of birth: " + dateOfBirth.Day + "/" + dateOfBirth.Month + "/" + dateOfBirth.Year);
+            Console.WriteLine("Height: " + healthProfile.CalculateHeight());
+            Console.WriteLine("Weight: " + weight + " lbs.");
+            Console.WriteLine();
+            Console.WriteLine("HEALTH PROFILE\n------------------------");
+            Console.WriteLine("Age: " + healthProfile.CalculateAge(dateOfBirth));
+            Console.WriteLine("BMI: " + healthProfile.BMI());
+            Console.WriteLine("Maximum heart rate: " + healthProfile.MaxHeartRate() + " beats per minute.");
+            Console.WriteLine("Target heart rate: " + healthProfile.MinTargetHeartRate() + "-" + healthProfile.MaxTargetHeartRate() + " beats per minute.");
+            Console.WriteLine();
+            Console.WriteLine("BMI VALUES");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("| Underweight: less than 18.5     |");
+            Console.WriteLine("| Normal: between 18.5 and 24.9   |");
+            Console.WriteLine("| Overweight: between 25 and 29.9 |");
+            Console.WriteLine("| Obese: 30 or greater            |");
+            Console.WriteLine("-----------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
